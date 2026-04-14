7590910075f672c92f07e436740ce53d718016c2
kittendevv <codingkitten@disroot.org>
2026-04-14 17:05:31 +0200

    Commit

---
+++ src/lib/widgets/navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { resolve } from '$app/paths';
+</script>
+
+<div class="rounded-full overflow-hidden m-2">
+    <div class="navbar bg-base-200">
+    <div class="flex-1 text-base-content font-black ml-4">
+        <a href={resolve('/')}>CodingKitten</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+        <li><a href={resolve('/')}>Link</a></li>
+        <li>
+            <details>
+            <summary>Parent</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+                <li><a href={resolve('/')}>Link 1</a></li>
+                <li><a href={resolve('/')}>Link 2</a></li>
+            </ul>
+            </details>
+        </li>
+        </ul>
+    </div>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Navbar from "$lib/widgets/navbar.svelte";
+</script>
+
+<div class="bg-[url($lib/assets/bg.png)] sepia bg-cover backdrop-blur-lg min-h-screen">
+    <div class="w-full h-full min-h-screen">
+       <Navbar />
+
+        <div class="container mx-auto w-full max-w-6xl rounded-xl overflow-hidden">
+            <div class="bg-base-100 p-4">
+                <h1 class="text-center text-6xl font-black py-5"> Hi I am CodingKitten </h1>
+            </div>
+        </div>     
+    </div>
+</div>
